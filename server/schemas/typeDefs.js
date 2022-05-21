eec10e3e17f7edd4e7edc4f586f4c91ab20458cd
laszlo-ratesic <keenanrc@hotmail.com>
2022-05-21 08:40:51 -0500

    resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,6 +40,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(content: BookDetails!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
